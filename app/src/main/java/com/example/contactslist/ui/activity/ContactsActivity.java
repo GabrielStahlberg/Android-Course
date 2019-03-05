a3a1a29e901fba2b0cfabf4e6664068d1f1c9ffe
GabrielStahlberg <gabrielstahlberg@gmaill.com>
2019-03-05 14:56:14 -0300

    Activiti FormStudent created.

--- app/src/main/java/com/example/contactslist/ui/activity/ContactsActivity.java
+++ app/src/main/java/com/example/contactslist/ui/activity/ContactsActivity.java
@@ -1,4 +1,4 @@
-package com.example.contactslist;
+package com.example.contactslist.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -6,20 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.contactslist.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ContactsActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(@Nullable Bundle saveInstanceState) {
     super.onCreate(saveInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_contacts);
     setTitle("Contacts");
     List<String> students = new ArrayList<>(Arrays.asList("Alex", "Smith", "John", "Eric", "Maxell"));
 
-    ListView studentsList = findViewById(R.id.activity_main_students_list);
+    ListView studentsList = findViewById(R.id.activity_contacts_list);
     studentsList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, students));
   }
 
---
+++ app/src/main/java/com/example/contactslist/ui/activity/FormStudentActivity.java
@@ -0,0 +1,15 @@
+package com.example.contactslist.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.contactslist.R;
+
+public class FormStudentActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_form_student);
+  }
+}
