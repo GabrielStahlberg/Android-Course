09f00e9eeaf52690862470f69cea317dec01df2d
GabrielStahlberg <gabrielstahlberg@gmaill.com>
2019-03-05 14:25:09 -0300

    Icon for FAB added and now extend AppCompatActivity instead of Activity, because with this class we can see the Bar on the top with a title.

--- app/src/main/java/com/example/contactslist/MainActivity.java
+++ app/src/main/java/com/example/contactslist/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.contactslist;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,13 +10,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(@Nullable Bundle saveInstanceState) {
     super.onCreate(saveInstanceState);
     setContentView(R.layout.activity_main);
-
+    setTitle("Contacts");
     List<String> students = new ArrayList<>(Arrays.asList("Alex", "Smith", "John", "Eric", "Maxell"));
 
     ListView studentsList = findViewById(R.id.activity_main_students_list);
